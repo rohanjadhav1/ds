b4ef575e7217e2f091ca235d8a94c44c3057f9f2
rohanjadhav1 <48465943+rohanjadhav1@users.noreply.github.com>
2021-11-04 05:16:37 -0700

    Add files via upload

---
+++ R/ds.R
@@ -0,0 +1,22 @@
+#' Creates histogram, boxplot and numeric summary
+#' @param x numeric variable
+#' @export
+
+
+ds <- function(x){
+  # 1 row and 2 columns
+  par(mfrow = c(1, 2))
+  # Histogram
+  hist(x, col = rainbow(30))
+  # Box plot
+  boxplot(x, col = "green")
+  # Back to original plane
+  par(mfrow = c(1, 1))
+
+  # Numeric Summary
+
+  data.frame(min = min(x),
+             median = median(x),
+             mean = mean(x),
+             max = max(x))
+}
